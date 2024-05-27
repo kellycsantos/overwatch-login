29d894da2b3b6be3a5af99722a3cba30ca6cf068
Kelly Santos <kellycristinasntos@gmail.com>
2024-05-26 21:15:39 -0300

    iniciando as configurações dos inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.scss'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.scss";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-   <main>
-<section role='image'></section>
-<section role='signup section'>
-  <h1>Login</h1>
-  <input type="text" /><input type="text" /><input type="text" /><input type="text" /><input type="text" /><input type="text" />
-</section>
-   </main>
+      <main>
+        <section role="image"></section>
+        <section role="signup section">
+          <h1>Login</h1>
+          <label>
+            First Name
+            <input type="text" />
+          </label>
+          <label>
+            Last Name
+            <input type="text" />
+          </label>
+          <label>
+            Email Address
+            <input type="text" />
+          </label>
+          <label>
+            Password
+            <input type="password" />
+          </label>
+          <label>
+            Confirm Password
+            <input type="password" />
+          </label>
+          <label>
+            <input type="checkout" />
+            I agree to the <a href="#">terms</a> and <a href="#">conditions</a>
+          </label>
+          <label>
+            <input type="text" />
+          </label>
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
