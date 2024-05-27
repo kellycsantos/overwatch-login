16028d63ae7dd65bc770faddef5e10781bcef05a
Kelly Santos <kellycristinasntos@gmail.com>
2024-05-26 21:59:49 -0300

    estrutura do login criada

--- src/App.jsx
+++ src/App.jsx
@@ -10,34 +10,46 @@ function App() {
     <>
       <main>
         <section role="image"></section>
-        <section role="signup section">
+        <section role="signup section" className="sign-up">
           <h1>Login</h1>
-          <label>
-            First Name
-            <input type="text" />
+            <section className="descrition">
+              <h3>create account</h3>
+              <p>Let's make the account first together</p>
+            </section>
+          <div className="input-row">
+            <label>
+              First Name
+              <input type="text" />
+            </label>
+            <label>
+              Last Name
+              <input type="text" />
+            </label>
+          </div>
+
+          <div className="input-row">
+            <label>
+              Email Address
+              <input type="email" />
+            </label>
+          </div>
+
+          <div className="input-row">
+            <label>
+              Password
+              <input type="password" />
+            </label>
+            <label>
+              Confirm Password
+              <input type="password" />
+            </label>
+          </div>
+          <label className="conditions">
+            <input type="checkbox" />I agree to the <a href="#"> terms </a> and
+            <a href="#">conditions</a>
           </label>
           <label>
-            Last Name
-            <input type="text" />
-          </label>
-          <label>
-            Email Address
-            <input type="text" />
-          </label>
-          <label>
-            Password
-            <input type="password" />
-          </label>
-          <label>
-            Confirm Password
-            <input type="password" />
-          </label>
-          <label>
-            <input type="checkout" />
-            I agree to the <a href="#">terms</a> and <a href="#">conditions</a>
-          </label>
-          <label>
-            <input type="text" />
+            <input type="submit" value="Create Account" />
           </label>
         </section>
       </main>
